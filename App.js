e13ed8903a09cc506f078c3387eb729198f7be14
quentg77 <q.guichaoua@gmail.com>
2019-09-17 13:08:46 +0200

    input ok

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import App from './src';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default function Main() {
+	return (
+		<PaperProvider>
+			<App />
+		</PaperProvider>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('main', () => Main);
---
+++ src/index.js
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { Dimensions, StatusBar } from "react-native";
+import { Text, TextInput, Button } from 'react-native-paper';
+import styled from 'styled-components';
+
+const MatchListComponent = ({ matchList }) => {
+	return (
+		<MatchListContainer>
+			{matchList.map(() => {
+				return <MatchBlock>
+
+				</MatchBlock>
+			})}
+		</MatchListContainer>
+	)
+}
+
+export default function Screen() {
+	StatusBar.setHidden(true);
+
+	const [RegExpEntrie, setRegExpEntrie] = useState(null);
+	const [ExempleTextEntrie, setExempleTextEntrie] = useState(null);
+
+	let matchList = []
+
+	useEffect(() => {
+		console.log('text change : ' + RegExpEntrie);
+	}, [RegExpEntrie])
+
+	const runRegexp = () => {
+		const reg = new RegExp(RegExpEntrie, "g");
+
+		matchList = [];
+
+		while ((match = reg.exec(ExempleTextEntrie)) !== null) {
+			console.log(match.index + ' ' + reg.lastIndex);
+			matchList.push({
+				text: match[0],
+				index: match.index,
+				lastIndex: reg.lastIndex
+			});
+		}
+
+		console.log(matchList);
+	}
+
+	return (
+		<Container>
+			<RegEntContainer>
+				<RegEntBlock>
+					<Text style={styles.bigGreyText}>/</Text>
+					<TextInput
+						label='RegExp'
+						mode='outlined'
+						value={RegExpEntrie}
+						onChangeText={text => setRegExpEntrie(text)}
+						style={styles.inputReg}
+					/>
+					<Text style={styles.bigGreyText}>/g</Text>
+				</RegEntBlock>
+			</RegEntContainer>
+
+			<ExempleTextContainer>
+				<TextInput
+					label='ExempleText'
+					mode='outlined'
+					multiline={true}
+					value={ExempleTextEntrie}
+					onChangeText={text => setExempleTextEntrie(text)}
+					style={styles.inputExText}
+				/>
+			</ExempleTextContainer>
+			<Button
+				mode="contained"
+				style={styles.buttonRun}
+				onPress={() => runRegexp()}
+			>
+				RUN
+			</Button>
+
+			<MatchListComponent matchList={matchList} />
+		</Container>
+	);
+}
+
+const getPercentHeight = (nb) => {
+	return (Math.floor(Dimensions.get('window').height * nb) / 100);
+}
+
+const getPercentWidth = (nb) => {
+	return (Math.floor(Dimensions.get('window').width * nb) / 100);
+}
+
+const Container = styled.View`
+	flex: 1;
+	align-items: center;
+`
+
+const RegEntContainer = styled.View`
+	height: ${getPercentHeight(20)};
+	width: ${getPercentWidth(80)};
+`
+
+const ExempleTextContainer = styled.View`
+	height: ${getPercentHeight(20)};
+	width: ${getPercentWidth(80)};
+`
+
+const MatchListContainer = styled.View`
+	width: ${getPercentWidth(80)};
+`
+
+const RegEntBlock = styled.View`
+	flex: 1;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+`
+
+const MatchBlock = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+`
+
+const styles = StyleSheet.create({
+	inputReg: {
+		margin: 5,
+		height: 60,
+		width: 200
+	},
+	inputExText: {
+		height: getPercentHeight(20),
+		width: getPercentWidth(80)
+	},
+	buttonRun: {
+		margin: 10,
+		height: 40,
+		width: 90
+	},
+	bigGreyText: {
+		margin: 5,
+		fontSize: 30,
+		color: "grey"
+	}
+})
